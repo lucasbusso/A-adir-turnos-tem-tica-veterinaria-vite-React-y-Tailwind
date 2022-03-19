8dca61cdd4a234b2f6a2c24c9504cd70f5c88c83
lucasbusso <lucasxbusso@gmail.com>
2022-03-19 11:43:24 -0300

    Hooks del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,19 @@
+import { useState, useEffect } from "react";
+
 const Formulario = () => {
+  const [nombre, setNombre] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [sintomas, setSintomas] = useState('');
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log('Enviando formulario')
+  }
+
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center">Seguimiento de pacientes</h2>
       <p className="text-lg mt-5 text-center mb-10">
         Añade los pacientes y {""}
@@ -8,7 +21,9 @@ const Formulario = () => {
         {""} su estado
         </p>
 
-        <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10" onSubmit={handleSubmit}>
+
+          
 
           <div className="mb-5">
             <label className="block text-gray-600 uppercase font-bold" htmlFor="mascota">
@@ -19,6 +34,8 @@ const Formulario = () => {
             type="text"
             placeholder="Nombre de tu mascota" 
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md hover:border-gray-300 focus:outline-none focus:border-indigo-600"
+            value={nombre}
+            onChange = { (e) => setNombre(e.target.value)}
             />
           </div>
 
@@ -31,6 +48,8 @@ const Formulario = () => {
             type="text"
             placeholder="Nombre del dueño" 
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md hover:border-gray-300 focus:outline-none focus:border-indigo-600"
+            value={propietario}
+            onChange = { (e) => setPropietario(e.target.value)}
             />
           </div>
 
@@ -43,6 +62,8 @@ const Formulario = () => {
             type="email"
             placeholder="Email de contacto" 
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md hover:border-gray-300 focus:outline-none focus:border-indigo-600"
+            value={email}
+            onChange = { (e) => setEmail(e.target.value)}
             />
           </div>
 
@@ -54,6 +75,8 @@ const Formulario = () => {
             id="alta"
             type="date" 
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md hover:border-gray-300 focus:outline-none focus:border-indigo-600"
+            value={fecha}
+            onChange = { (e) => setFecha(e.target.value)}
             />
           </div>
 
@@ -68,6 +91,8 @@ const Formulario = () => {
             rows="10" 
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md hover:border-gray-300 focus:outline-none focus:border-indigo-600" 
             placeholder="Escribe aquí los sintomas">
+            value={sintomas}
+            onChange = { (e) => setSintomas(e.target.value)}
             </textarea>
           </div>
 
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,6 +1,6 @@
 const Paciente = () => {
   return (
-    <div className="m-5 bg-white shadow-md px-5 py-10 rounded-lg ">
+    <div className="mx-5 bg-white shadow-md px-5 py-10 rounded-lg">
 
         <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {""}
         <span className="font-normal normalcase">        </span>
