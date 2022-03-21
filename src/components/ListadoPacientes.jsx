8115ce3d7241266894708fff69c9a35c5309f8a3
Lucas Busso <lucasxbusso@gmail.com>
2022-03-21 16:15:52 -0300

    Header de listadoPacientes condicional

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -5,19 +5,35 @@ const ListadoPacientes = ({pacientes}) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
 
-      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-      Administra tus {""}
-      <span className="text-indigo-600 font-bold">pacientes y citas</span>
-      </p>
+ {/* Re escribe el heading del listado pacientes. Cambia cuando ingresas el primer paciente */}
+      { pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+          Administra tus {''}
+          <span className="text-indigo-600 font-bold">pacientes y citas</span>
+          </p>
 
-      {pacientes.map( paciente => (
-        <Paciente 
-//Siempre utilizar key cuando usas .map
-          key= {paciente.id}
-          paciente={paciente}
-        />
-      ))}
+          { pacientes.map( paciente => (
+            <Paciente 
+ // Siempre utilizar key cuando usas .map
+              key= {paciente.id}
+              paciente={paciente}
+            />
+          ))}  
+        </>
+
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado de Pacientes vacío</h2>
+            <p className="text-xl mt-5 mb-10 text-center">
+            Completa el formulario y podrás ver tus  {''}
+            <span className="text-indigo-600 font-bold">pacientes y citas aquí</span>
+            </p>
+        </>
+      )}
+
+      
     </div>
   )
 }
