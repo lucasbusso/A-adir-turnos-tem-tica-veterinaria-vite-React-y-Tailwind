46b37411da97e77e5ac49f0cc5c4e853969e1b1c
Lucas Busso <lucasxbusso@gmail.com>
2022-03-21 14:33:58 -0300

    Props de pacientes

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
-const Formulario = () => {
+const Formulario = ({pacientes, setPacientes}) => {
+//Componentes del formulario
   const [nombre, setNombre] = useState('');
   const [propietario, setPropietario] = useState('');
   const [email, setEmail] = useState('');
@@ -11,17 +12,33 @@ const Formulario = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-
-    //Validación del formulario
+//Validación del formulario
     if([nombre, propietario, email, fecha, sintomas].includes('')){
       setError(true)
       return;
-    } 
+    }
     setError(false)
+//Objeto de paciente
+    const objetoPaciente = {
+      nombre,
+      propietario,
+      email,
+      fecha,
+      sintomas
+    }
+    setPacientes([...pacientes, objetoPaciente]);
+//Reiniciar el formualrio
+    setNombre('')
+    setPropietario('')
+    setEmail('')
+    setFecha('')
+    setSintomas('')
   }
 
   return (
+
     <div className="md:w-1/2 lg:w-2/5 mx-5">
+{/* Headers */}
       <h2 className="font-black text-3xl text-center">Seguimiento de pacientes</h2>
       <p className="text-lg mt-5 text-center mb-10">
         Añade los pacientes y {""}
@@ -29,85 +46,85 @@ const Formulario = () => {
         {""} su estado
         </p>
 
-        <form 
-        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10" 
+        <form
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
         onSubmit={handleSubmit}
         >
-          
+{/* Mascota */}
           <div className="mb-5">
             <label className="block text-gray-600 uppercase font-bold" htmlFor="mascota">
             Nombre de la Mascota
             </label>
-            <input 
+            <input
             id="mascota"
             type="text"
-            placeholder="Nombre de tu mascota" 
+            placeholder="Nombre de tu mascota"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md hover:border-gray-300 focus:outline-none focus:border-indigo-600"
             value={nombre}
             onChange = { (e) => setNombre(e.target.value)}
             />
           </div>
-
+{/* Dueño */}
           <div className="mb-5">
             <label className="block text-gray-600 uppercase font-bold" htmlFor="propietario">
             Nombre del Dueño
             </label>
-            <input 
+            <input
             id="propietario"
             type="text"
-            placeholder="Nombre del dueño" 
+            placeholder="Nombre del dueño"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md hover:border-gray-300 focus:outline-none focus:border-indigo-600"
             value={propietario}
             onChange = { (e) => setPropietario(e.target.value)}
             />
           </div>
-
+{/* Email */}
           <div className="mb-5">
             <label className="block text-gray-600 uppercase font-bold" htmlFor="email">
             Email
             </label>
-            <input 
+            <input
             id="email"
             type="email"
-            placeholder="Email de contacto" 
+            placeholder="Email de contacto"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md hover:border-gray-300 focus:outline-none focus:border-indigo-600"
             value={email}
             onChange = { (e) => setEmail(e.target.value)}
             />
           </div>
-
+{/* Fecha de alta */}
           <div className="mb-5">
             <label className="block text-gray-600 uppercase font-bold" htmlFor="alta">
            Alta
             </label>
-            <input 
+            <input
             id="alta"
-            type="date" 
+            type="date"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md hover:border-gray-300 focus:outline-none focus:border-indigo-600"
             value={fecha}
             onChange = { (e) => setFecha(e.target.value)}
             />
           </div>
-
+{/* Sintomas */}
           <div className="">
             <label className="block text-gray-600 uppercase font-bold" htmlFor="sintomas">
             Describe los síntomas
             </label>
-            <textarea 
-            id="sintomas" 
-            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md hover:border-gray-300 focus:outline-none focus:border-indigo-600" 
+            <textarea
+            id="sintomas"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md hover:border-gray-300 focus:outline-none focus:border-indigo-600"
             placeholder="Escribe aquí los sintomas"
             value={sintomas}
             onChange = { (e) => setSintomas(e.target.value)}>
             </textarea>
           </div>
-
 {/* Mensaje de error */}
           { error && (
-          <div className="text-red-600 text-left text-sm p-3 uppercase font-bold mb-3 rounded-md mb-3">
+          <div className="text-red-600 text-left text-sm p-3 uppercase font-semibold mb-3 rounded-md mb-3">
             <p>Todos los campos son obligatorios</p>
           </div>) }
-          <input 
+{/* Boton de enviar */}
+          <input
           type="submit"
           className="bg-indigo-600 w-full p-3 text-white uppercase font-bold rounded-md shadow-lg
           hover:bg-indigo-700 cursor-pointer transition-all"
